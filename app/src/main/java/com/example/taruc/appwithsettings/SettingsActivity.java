ca71015c5f483b5b1411d4b65546bfdfda505f19
LimJianHao <jianhao127@hotmail.cim>
2018-11-28 09:48:12 +0800

    Update

---
+++ app/src/main/java/com/example/taruc/appwithsettings/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.taruc.appwithsettings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
